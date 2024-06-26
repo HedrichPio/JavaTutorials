50e413185f444ab317d5e6da644bc24ef6bcae98
HedrichPio <mhpiofernando1@gmail.com>
2024-06-26 21:27:05 +0200

    Used Jakarta CDI and XML configuration

---
+++ src/main/java/com/hedrich/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -0,0 +1,26 @@
+package com.hedrich.learnspringframework.examples.h1;
+
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hedrich.learnspringframework.game.GameRunner;
+
+
+
+public class XmlConfigurationContextLauncherApplication {
+	
+	public static void main(String[] args) {
+		
+		try(var context = new ClassPathXmlApplicationContext("contextConfiguration.xml")){
+			
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			
+			context.getBean(GameRunner.class).run();  
+		};
+	}
+}
+ 
